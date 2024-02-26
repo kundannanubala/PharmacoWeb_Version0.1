e27fa0f885ba901e814da515b4436c4edf103553
SAFIUDDIN SHAIK <113826224+SAFIUDDIN-SHAIK@users.noreply.github.com>
2024-02-26 18:25:11 +0530

    Added border to button

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -13,7 +13,7 @@ const Homepage = () => {
           <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Our Prescription Checker tool goes beyond providing detailed information about your medications. It actively checks for potential drug-drug interactions (DDIs), ensuring your prescription safety and contributing to pharmacovigilance.</p>
           
           <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-            <button onClick={() => {window.location.href = '/MultiSearchTablets';}} className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900 ">
+            <button onClick={() => {window.location.href = '/MultiSearchTablets';}} className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900 border-solid border-2 border-sky-500">
               Start Checking Prescriptions
               <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
             </button>
